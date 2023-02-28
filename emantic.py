6b769c4f304b2448029b5eca289b448c1de05ef2
Sirdavinius <69756332+Sirdavinius@users.noreply.github.com>
2023-02-28 18:47:49 +0000

    Add files via upload

---
+++ emantic.py
@@ -0,0 +1,6 @@
+#similarities between cat, monkey and banana
+#=> i found it really interesting how monkey and banana had a higher similarity score than cat and banana.
+
+#difference between en_core_web_sm and en_core_web_md
+#it was interesting to see the write out 'the model has no vectors loaded'
+#i assume vectors link words further than just based on type of word.
