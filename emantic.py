4529013b80e88408a4c22ec798dd0dd2eb35bdeb
Sirdavinius <69756332+Sirdavinius@users.noreply.github.com>
2023-02-28 18:51:25 +0000

    Update emantic.py

--- emantic.py
+++ emantic.py
@@ -1,5 +1,5 @@
 #similarities between cat, monkey and banana
-#=> i found it really interesting how monkey and banana had a higher similarity score than cat and banana.
+#=> i found it really interesting how monkey and banana had a higher similarity score than cat and banana. this is especially interesting as this is easy for a human to see but to make a computer program do this is fascinating.
 
 #difference between en_core_web_sm and en_core_web_md
 #it was interesting to see the write out 'the model has no vectors loaded'
